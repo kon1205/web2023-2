2e4d533b7a336bb15c663d8741498818fa0035ab
kon1205 <131091843+kon1205@users.noreply.github.com>
2023-10-24 10:14:58 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -5,4 +5,8 @@ function sayhello65(){alert('Hello,'+who65+'!');}
 function taro65(){who65='太郎';}
 function hanako65(){who65='花子';}
 
+function sayhello66(){alert(who66+'が来た!');}
+function taro66(){who65='太郎';}
+function hanako66(){who65='花子';}
+
 
