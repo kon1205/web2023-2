8be6edac2b9964a79ea26ed92be0dc0bcb978c63
kon1205 <131091843+kon1205@users.noreply.github.com>
2023-10-31 09:54:00 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -3,8 +3,8 @@ function sayhello68(){
 '!');
 }
 function taro68(){
- document.getElementByld('who68').innerHTML +'太郎';
+ document.getElementByld('who68').innerHTML = '太郎';
 }
 function hanako68(){
- document.getElementByld('who68').innerHTML +'花子';
+ document.getElementByld('who68').innerHTML = '花子';
 }
