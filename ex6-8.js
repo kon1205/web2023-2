42f3e52bc9b1fd93be1c5c4c3378668c6c736d67
kon1205 <131091843+kon1205@users.noreply.github.com>
2023-11-07 09:48:47 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -1,13 +1,12 @@
-function sayhello68(){
-  alert('Hello, ' + document.getElementByld('who68').innerHTML +
-'!');
-}
-function taro68(){
- document.getElementByld('who68').innerHTML = '太郎';
-}
-function hanako68(){
- document.getElementByld('who68').innerHTML = '花子';
-}
+function sayhello68()
+{alert('Hello, ' + document.getElementById('who68').innerHTML + '!');
+    }
+function taro68()
+ {document.getElementById('who68').innerHTML = '太郎';
+    }
+function hanako68()
+  {document.getElementById('who68').innerHTML = '花子';
+    }
 function gazo688()
 {document.getElementByld('who688').innerHTML=
   '<img src="./image/1031.jpg alt="ミツバチ">'
