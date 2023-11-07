787586c9ee53310d7ac43100217df121cc820b55
kon1205 <131091843+kon1205@users.noreply.github.com>
2023-11-07 09:36:25 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -8,3 +8,7 @@ function taro68(){
 function hanako68(){
  document.getElementByld('who68').innerHTML = '花子';
 }
+function gazo688()
+{document.getElementByld('who688').innerHTML=
+  '<img src="./image/1031.jpg alt="ミツバチ">'
+ ;}
