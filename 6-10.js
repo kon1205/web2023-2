b24ee400ad4d84b02a60aac77c61c2cd003cbf49
kon1205 <131091843+kon1205@users.noreply.github.com>
2023-11-14 10:21:12 +0900

    Update 6-10.js

--- 6-10.js
+++ 6-10.js
@@ -1,7 +1,3 @@
-function imgchange1() {
-document.getElementById('logo').src = 
-'./image/1030.png';
+function imgchange1() {document.getElementById('logo').src = './image/1030.png';}
 
-function imgchange1() {
-document.getElementById('logo').src = 
-'./image/1031.png';
+function imgchange1() {document.getElementById('logo').src = './image/1031.png';}
