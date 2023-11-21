6d3388a329324205e8913bc2f206070d353c6841
kon1205 <131091843+kon1205@users.noreply.github.com>
2023-11-21 09:30:27 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,9 @@
-function imgchange1(){document.getElementById('mitubati').src='./image/8.jpg';}
-function imgchange2(){document.getElementById('hana').src='./image/1030.jpg';}
+function imgchange1(){
+  document.getElementById('mitubati').src =
+    './image/8.png';
+}
+
+function imgchange2(){
+  document.getElementById('hana').src =
+    './image/1030.png';
+}
