970ac094137f827a4d36a61b9f95d2c4d99052df
kon1205 <131091843+kon1205@users.noreply.github.com>
2023-11-28 10:17:24 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,2 +1,2 @@
-function imgchange1(){ document.getElementById('logo').src='./image/ju.jpg';}
-function imgchange2(){ document.getElementById('logo').src='./image/jo.jpg';}
+function imgchange1(){document.getElementById('logo').src='./image/ju.jpg';}
+function imgchange2(){document.getElementById('logo').src='./image/jo.jpg';}
