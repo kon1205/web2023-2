a0e4d95a780e3c3d1a9d64da44c635f809c0fbcf
kon1205 <131091843+kon1205@users.noreply.github.com>
2023-12-05 10:16:52 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,2 +1,2 @@
-function imgchange3(){document.getElementById('jojo').src='./image/ju.jpg';}
-function imgchange4(){document.getElementById('jojo').src='./image/jo.jpg';}
+ function imgchange3(){document.getElementById('jojo').src='./image/ju.jpg';}
+ function imgchange4(){document.getElementById('jojo').src='./image/jo.jpg';}
