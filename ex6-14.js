02d2c73f14b5f50235292e23ca27d8634206bcf2
kon1205 <131091843+kon1205@users.noreply.github.com>
2024-01-29 01:23:59 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,3 +1,6 @@
-var img;
-function imgchange5(){ document.getElementById('arashi').src='./image/arashi.jpg'; setTimeout("imgchange6()",3000);}
-function imgchange6(){ document.getElementById('arashi').src='./image/usiro.jpg'; }
+function imgchange5(){
+  document.getElementById('arashi').src= './image/arashi.jpg'; 
+}
+function imgchange6(){
+  document.getElementById('arashi').src='./image/usiro.jpg'; 
+}
